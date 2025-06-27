e5ca88aaa74aeb11e6105874187640f46173f72d
thiagomullergomes <fanatic.habbo@gmail.com>
2025-06-27 15:57:46 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/medassist-ai-page-33/",
   server: {
     host: "::",
     port: 8080,
